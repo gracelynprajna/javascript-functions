03d96df246ecd24a4a2faefe5228b69a71be514c
Gracelyn Prajna <gracelynprajna@gmail.com>
2022-10-05 20:19:09 -0400

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+function setDimension(){
+    document.getElementById("container").style.height="100vh"
+    document.getElementById("container").style.width="100vw"
+}
+
+setDimension()
+
+function setColor(){
+    document.getElementById("one").style.backgroundColor = "lightblue"
+    document.getElementById("two").style.backgroundColor = "pink"
+    document.getElementById("three").style.backgroundColor = "orange"
+}
+
+setColor()
